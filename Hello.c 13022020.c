df01b12de648902f38a005c4926a2de8ddbf222b
victor20000202 <60487093+victor20000202@users.noreply.github.com>
2020-02-13 15:20:29 -0600

    Create Hello.c 13022020.c

---
+++ Hello.c 13022020.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define num 5
+
+int main(){
+	//"Declaracion de variables"//
+   const int constante=100; // constante//
+   int variable=200; //variable//
+   
+   //"Impresion"//
+   printf("\tPrimeros valores\n\n");
+   printf("El valor de variable es:%d\n", variable);
+   printf("El valor de constante es:%d\n",constante);
+   printf("El valor define es: %d\n", num);
+   
+   return 0;
+}
